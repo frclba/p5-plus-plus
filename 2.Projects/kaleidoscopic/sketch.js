32505383d9350ca336a911b245d1a6f84eb4b505
Fernando Aguilar <fernando@aguilar.net.br>
2018-03-19 00:16:50 +0200

    Refactoring variable names kaleidoscopic

--- 2.Projects/kaleidoscopic/sketch.js
+++ 2.Projects/kaleidoscopic/sketch.js
@@ -13,22 +13,20 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    var an = atan2(mouseY-height/2, mouseX-width/2);
-    var d = dist(mouseX, mouseY, width/2, height/2);
+    var angle = atan2(mouseY-height/2, mouseX-width/2);
+    var distance = dist(mouseX, mouseY, width/2, height/2);
     if (oldAngle != -100) {
        translate(width/2, height/2);
-      for (var a=0; a<TWO_PI; a += pas) {
-        line(cos(oldAngle+a)*oldDist, sin(oldAngle+a)*oldDist, cos(an+a)*d, sin(an+a)*d);
+      for (var i = 0; i < TWO_PI; i += pas) {
+        line(cos(oldAngle + i) * oldDist, sin(oldAngle + i) * oldDist, cos(angle + i) * distance, sin(angle + i) * distance);
       }
     }
-    oldAngle = an;
-    oldDist=d;
+    oldAngle = angle;
+    oldDist = distance;
  } else {
-  oldAngle=-100;
+  oldAngle =- 100;
  }
 }
-
-
 
 function mousePressed() {
   oldAngle = atan2(mouseY-height/2, mouseX-width/2);
@@ -38,13 +36,13 @@ function mousePressed() {
 
 
 function keyReleased() {
-  if (key=='+') {
+  if (key == '+') {
     n++;
-  } else if (key=='-') {
+  } else if (key == '-') {
     n--;
   } else if (keyCode==10) {
     background(255);
   }
-    n=constrain(n, 1, 200);
-    pas=TWO_PI/n;
+    n = constrain(n, 1, 200);
+    pas = TWO_PI/n;
 }
