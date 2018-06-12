37dacfef19e1ae9016229b77681eb6490898ec68
Fernando Aguilar <fernando@aguilar.net.br>
2018-06-12 17:39:44 -0300

    Circle fractals

--- The_Nature_Of_Code/kadenze/session_04/fractal/sketch.js
+++ The_Nature_Of_Code/kadenze/session_04/fractal/sketch.js
@@ -4,4 +4,19 @@ function setup() {
 
 function draw() {
 
+	drawElipse(width/2, height/2, 200);
+}
+
+
+
+function drawElipse(x, y, d){
+	ellipse(x, y, d);
+	ellipse(x+400, y/2, d*2);
+	ellipse(x/2, y/2, d*2);
+
+	if(d > 10){
+		drawElipse(x + d, y, d * 0.333);
+		drawElipse(x - d, y, d * 0.333);
+	}
+
 }
