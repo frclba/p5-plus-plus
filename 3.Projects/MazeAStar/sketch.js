5b70eecd505a116f7d83bf719ad00957038b4454
Fernando Aguilar <fernando@aguilar.net.br>
2018-07-14 00:08:46 -0300

    A* Maze - Cleaning up code

--- 3.Projects/MazeAStar/sketch.js
+++ 3.Projects/MazeAStar/sketch.js
@@ -1,18 +1,3 @@
-function removeFromArray(arr, elt) {
-  for (var i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] == elt) {
-      arr.splice(i, 1);
-    }
-  }
-}
-
-function heuristic(a, b) {
-  var d = dist(a.i, a.j, b.i, b.j);
-  // var d = abs(a.i - b.i) + abs(a.j - b.j);
-  return d;
-}
-
-
 var cols = 50;
 var rows = 50;
 var grid = new Array(cols);
@@ -24,67 +9,8 @@ var end;
 var w, h;
 var path = [];
 
-function Spot(i, j) {
-  this.i = i;
-  this.j = j;
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-  this.neighbors = [];
-  this.previous = undefined;
-  this.wall = false;
-
-  if (random(1) < 0.3) {
-    this.wall = true;
-  }
-
-
-  this.show = function(col) {
-    //fill(col);
-    if (this.wall) {
-      fill(0);
-      noStroke();
-      ellipse(this.i * w + w / 2, this.j * h + h / 2, w / 2, h / 2);
-    }
-    //rect(this.i * w, this.j * h, w - 1, h - 1);
-  }
-
-  this.addNeighbors = function(grid) {
-    var i = this.i;
-    var j = this.j;
-    if (i < cols - 1) {
-      this.neighbors.push(grid[i + 1][j]);
-    }
-    if (i > 0) {
-      this.neighbors.push(grid[i - 1][j]);
-    }
-    if (j < rows - 1) {
-      this.neighbors.push(grid[i][j + 1]);
-    }
-    if (j > 0) {
-      this.neighbors.push(grid[i][j - 1]);
-    }
-    if (i > 0 && j > 0) {
-      this.neighbors.push(grid[i - 1][j - 1]);
-    }
-    if (i < cols - 1 && j > 0) {
-      this.neighbors.push(grid[i + 1][j - 1]);
-    }
-    if (i > 0 && j < rows - 1) {
-      this.neighbors.push(grid[i - 1][j + 1]);
-    }
-    if (i < cols - 1 && j < rows - 1) {
-      this.neighbors.push(grid[i + 1][j + 1]);
-    }
-  }
-
-
-
-
-}
-
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640,640);
   console.log('A*');
 
   w = width / cols;
@@ -107,24 +33,18 @@ function setup() {
     }
   }
 
-
-
   start = grid[0][0];
   end = grid[cols - 1][rows - 1];
   start.wall = false;
   end.wall = false;
 
   openSet.push(start);
-
-
-
-
 }
 
 function draw() {
+  background(255);
 
-  if (openSet.length > 0) {
-
+  if (openSet.length > 0) {     // we can keep going
     var winner = 0;
     for (var i = 0; i < openSet.length; i++) {
       if (openSet[i].f < openSet[winner].f) {
@@ -132,12 +52,10 @@ function draw() {
       }
     }
     var current = openSet[winner];
-
     if (current === end) {
       noLoop();
       console.log("DONE!");
     }
-
 
     removeFromArray(openSet, current);
     closedSet.push(current);
@@ -167,36 +85,13 @@ function draw() {
           neighbor.previous = current;
         }
       }
-
     }
-    // we can keep going
-  } else {
+  } else {     // no solution
     console.log('no solution');
     noLoop();
     return;
-
-    // no solution
   }
 
-  background(255);
-
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      grid[i][j].show(color(255));
-    }
-  }
-
-
-  for (var i = 0; i < closedSet.length; i++) {
-    //closedSet[i].show(color(255, 0, 0));
-  }
-
-  for (var i = 0; i < openSet.length; i++) {
-    //openSet[i].show(color(0, 255, 0));
-  }
-
-
-  // Find the path
   path = [];
   var temp = current;
   path.push(temp);
@@ -206,10 +101,34 @@ function draw() {
   }
 
 
-  for (var i = 0; i < path.length; i++) {
-    //path[i].show(color(0, 0, 255));
-  }
+  // Find the path
+  findPath();
+  render();
 
+}
+
+
+function render(){
+  // Show white spaces
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show(color(255));
+    }
+  }
+  // Show Blocked Visited Path
+  // for (var i = 0; i < closedSet.length; i++) {
+  //   closedSet[i].show(color(255, 0, 0));
+  // }
+  // Show Possible Path
+  // for (var i = 0; i < openSet.length; i++) {
+  //   openSet[i].show(color(0, 255, 0));
+  // }
+  // Show Current best path
+  // for (var i = 0; i < path.length; i++) {
+  //   path[i].show(color(0, 0, 255));
+  // }
+
+  //Show  Current Best path in line
   noFill();
   stroke(255, 0, 200);
   strokeWeight(w / 2);
@@ -218,4 +137,18 @@ function draw() {
     vertex(path[i].i * w + w / 2, path[i].j * h + h / 2);
   }
   endShape();
+}
+
+function removeFromArray(arr, elt) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] == elt) {
+      arr.splice(i, 1);
+    }
+  }
+}
+
+function heuristic(a, b) {
+  var d = dist(a.i, a.j, b.i, b.j);
+  // var d = abs(a.i - b.i) + abs(a.j - b.j);
+  return d;
 }
