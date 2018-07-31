be4c754090220e7b9eb9510c341d27566d31f001
Fernando Aguilar <fernando@aguilar.net.br>
2018-07-31 16:56:22 -0300

    Improving neural network animation

--- IntelligenceAndLearning/session4/neural_network_animation/sketch.js
+++ IntelligenceAndLearning/session4/neural_network_animation/sketch.js
@@ -8,26 +8,36 @@ function setup(){
   //Inputs
   let in0 = new Neuron(-200, -75);
   let in1 = new Neuron(-200, 75);
+  let in2 = new Neuron(-200, -150);
   //Hidden
   let hid0 = new Neuron(0, -75);
   let hid1 = new Neuron(0, 75);
-
   //Output
   let out = new Neuron(200, 0);
 
-
+  //INPUT - HIDDEN
+  // 0
   network.connect(in0, hid0);
   network.connect(in0, hid1);
+  // 1
   network.connect(in1, hid0);
   network.connect(in1, hid1);
+ // 2
+  network.connect(in2, hid0);
+  network.connect(in2, hid1);
+
+  //HIDDEN - OUTPUTs
   network.connect(hid0, out);
   network.connect(hid1, out);
 
-
+  //INPUT
   network.addNeuron(in0);
   network.addNeuron(in1);
+  network.addNeuron(in2);
+  //HIDDEN
   network.addNeuron(hid0);
   network.addNeuron(hid1);
+  //OUTPUT
   network.addNeuron(out);
 }
 
@@ -36,8 +46,9 @@ function draw(){
   translate(width/2, height/2);
   network.show();
   network.update();
+
   if (frameCount % 30 == 0){
-    let inputs = [random(1), random(1)]
+    let inputs = [random(1), random(1), random(1)]
     network.feedforward(inputs);
   }
 }
