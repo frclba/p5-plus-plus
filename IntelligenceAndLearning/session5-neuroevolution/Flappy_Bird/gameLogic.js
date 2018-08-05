e495af0ea5ea3aee204c9504e8b798b007082e42
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-05 15:16:12 -0300

    Refactoring and optimizing code for Flappy Bird

---
+++ IntelligenceAndLearning/session5-neuroevolution/Flappy_Bird/gameLogic.js
@@ -0,0 +1,88 @@
+
+function createBirds(){
+  for(let i = 0; i < totalPopulation; i++){
+    let bird = new Bird();
+    activeBirds[i] = bird;
+    allBirds[i] = bird;
+  }
+}
+
+function updatePipes(){
+  for(let i = pipes.length - 1; i >= 0; i--){
+    pipes[i].update();
+    if(pipes[i].offscreen()){
+      pipes.splice(i, 1);
+    }
+  }
+}
+
+function controlActiveBirds(){
+  for(let i = activeBirds.length - 1; i >= 0; i--){
+    let bird = activeBirds[i];
+    bird.think(pipes);
+    bird.update();
+
+    for(let j = 0; j < pipes.length; j++){
+      if(pipes[j].hits(activeBirds[i])){
+        activeBirds.splice(i, 1);
+        break;
+      }
+    }
+    if(bird.bottomTop()){
+      activeBirds.splice(i, 1);
+    }
+  }
+}
+
+
+function controlBestBird(){
+  bestBird.think(pipes);
+  bestBird.update();
+  for(let j = 0; j < pipes.length; j++){
+    if(pipes[j]. hits(bestBird)){
+      resetGame();
+      break;
+    }
+  }
+  if(bestBird.bottomTop()){
+    resetGame();
+  }
+}
+
+
+function showActiveBirds(){
+  for(let i = 0; i < activeBirds.length; i++){
+    activeBirds[i].show();
+  }
+  if(activeBirds.length == 0){
+    nextGeneration();
+  }
+}
+
+function defineHighScore(){
+  let tempHighScore = 0;
+  if(!runBest){
+    let tempBestBird = null;
+    for(let i = 0; i < activeBirds.length; i++){
+      let s = activeBirds[i].score;
+      if(s > tempHighScore){
+        tempHighScore = s;
+        tempBestBird = activeBirds[i];
+      }
+    }
+
+    if(tempHighScore > highScore){
+      highScore = tempHighScore;
+      bestBird = tempBestBird;
+    }
+  }
+  else{
+    tempHighScore = bestBird.score;
+    if(tempHighScore > highScore){
+      highScore = tempHighScore;
+    }
+  }
+
+  highScoreSpan.html(tempHighScore);
+  allTimeHighScoreSpan.html(highScore);
+}
--- IntelligenceAndLearning/session5-neuroevolution/Flappy_Bird/sketch.js
+++ IntelligenceAndLearning/session5-neuroevolution/Flappy_Bird/sketch.js
@@ -1,3 +1,10 @@
+/*
+*TODO ->
+** maybe // OPTIMIZE:
+** Insert Images and other stuff
+*
+*/
+
 let totalPopulation = 500;
 
 let activeBirds = [];
@@ -29,20 +36,41 @@ function setup(){
 }
 
 
+
+function draw(){
+  background(0);
+  let cycles = speedSlider.value();
+  speedSpan.html(cycles);
+
+  for(let n = 0; n < cycles; n++){
+    updatePipes();
+
+    if(runBest)      controlBestBird();
+    else             controlActiveBirds();
+
+    if(counter % 75 == 0){
+      pipes.push(new Pipe());
+    }
+    counter++;
+  }
+
+  defineHighScore();
+
+  for(let i = 0; i < pipes.length; i++){
+    pipes[i].show();
+  }
+
+  if(runBest)     bestBird.show();
+  else            showActiveBirds();
+}
+
+
 function selectDOMs(){
   speedSlider = select('#speedSlider');
   speedSpan = select('#speedSpan');
   highScoreSpan = select('#highScoreSpan');
   allTimeHighScoreSpan = select('#allTimeHighScoreSpan');
-  runBestButton = select('#speedSlider');
-}
-
-function createBirds(){
-  for(let i = 0; i < totalPopulation; i++){
-    let bird = new Bird();
-    activeBirds[i] = bird;
-    allBirds[i] = bird;
-  }
+  runBestButton = select('#best');
 }
 
 function toggleState(){
@@ -53,99 +81,5 @@ function toggleState(){
   } else{
     nextGeneration();
     runBestButton.html('run best');
-  }
-}
-
-
-function draw(){
-  background(0);
-  let cycles = speedSlider.value();
-  speedSpan.html(cycles);
-
-  for(let n = 0; n < cycles; n++){
-    for(let i = pipes.length - 1; i >= 0; i--){
-      pipes[i].update();
-      if(pipes[i].offscreen()){
-        pipes.splice(i, 1);
-      }
-    }
-
-    if(runBest){
-      bestBird.think(pipes);
-      bestBird.update();
-      for(let j = 0; j < pipes.length; j++){
-        if(pipes[j]. hits(bestBird)){
-          resetGame();
-          break;
-        }
-      }
-      if(bestBird.bottomTop()){
-        resetGame();
-      }
-    }
-    else{
-      for(let i = activeBirds.length - 1; i >= 0; i--){
-        let bird = activeBirds[i];
-        bird.think(pipes);
-        bird.update();
-
-        for(let j = 0; j < pipes.length; j++){
-          if(pipes[j].hits(activeBirds[i])){
-            activeBirds.splice(i, 1);
-            break;
-          }
-        }
-        if(bird.bottomTop()){
-          activeBirds.splice(i, 1);
-        }
-      }
-    }
-
-    if(counter % 75 == 0){
-      pipes.push(new Pipe());
-    }
-    counter++;
-  }
-
-
-  let tempHighScore = 0;
-  if(!runBest){
-    let tempBestBird = null;
-    for(let i = 0; i < activeBirds.length; i++){
-      let s = activeBirds[i].score;
-      if(s > tempHighScore){
-        tempHighScore = s;
-        tempBestBird = activeBirds[i];
-      }
-    }
-
-    if(tempHighScore > highScore){
-      highScore = tempHighScore;
-      bestBird = tempBestBird;
-    }
-  }
-  else{
-    tempHighScore = bestBird.score;
-    if(tempHighScore > highScore){
-      highScore = tempHighScore;
-    }
-  }
-
-  highScoreSpan.html(tempHighScore);
-  allTimeHighScoreSpan.html(highScore);
-
-  for(let i = 0; i < pipes.length; i++){
-    pipes[i].show();
-  }
-
-  if(runBest){
-    bestBird.show();
-  }  else{
-    for(let i = 0; i < activeBirds.length; i++){
-      activeBirds[i].show();
-    }
-    if(activeBirds.length == 0){
-      nextGeneration();
-    }
   }
 }
