016b520efca7da10b7c348d78b25dcc9a49d7a43
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-10 07:38:00 -0300

    Changing 1.04 to ES8

--- 1.Projects/04.JSON_async_await/sketch.js
+++ 1.Projects/04.JSON_async_await/sketch.js
@@ -4,15 +4,34 @@ let giphyAPI = "https://api.giphy.com/v1/gifs/search?rating=G&api_key=dc6zaTOxFJ
 function setup() {
 	noCanvas();
 
-	fetch(wordnikAPI)
-		.then(response => response.json())
-		.then(json => {
-			createP(json.word);
-			return fetch(giphyAPI + json.word);
-		})
-		.then(response => response.json())
-		.then(json => {
-			createImg(json.data[0].images['fixed_height_small'].url)
-		})
-		.catch(err => console.error(err));
+	wordGif().then(results =>{
+		createP(results.word);
+		createImg(results.img);
+	}).catch(err => console.error(err));
+
+	// fetch(wordnikAPI)
+	// 	.then(response => response.json())
+	// 	.then(json => {
+	// 		createP(json.word);
+	// 		return fetch(giphyAPI + json.word);
+	// 	})
+	// 	.then(response => response.json())
+	// 	.then(json => {
+	// 		createImg(json.data[0].images['fixed_height_small'].url)
+	// 	})
+	// 	.catch(err => console.error(err));
+}
+
+
+async function wordGif(){
+	let responseWord = await fetch(wordnikAPI);
+	let jsonWord = await responseWord.json();
+	let responseGif = await fetch(giphyAPI + jsonWord.word);
+	let jsonGif = await responseGif.json();
+	let img_url = jsonGif.data[0].images['fixed_height_small'].url
+
+	return {
+		word: jsonWord.word,
+		img: img_url
+	}
 }
