84719ba7ae3e9caeae9a507d6e974a10df277326
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-02 12:09:06 -0300

    2.07 - Finished

--- 2.Projects/07.Organic-Iris/iris.js
+++ 2.Projects/07.Organic-Iris/iris.js
@@ -1,72 +1,56 @@
-let circles = [];
-let protection = 100;
 
-let angle = 0;
-let NDo;
-let isOutside = false;
-
-let noiseScale;
-let noiseStrength;
 
 class Iris {
   // x,y    = the current position
   // ox,oy  = the position, but slightly back in time
-  // sx,sy  = start positions
-  constructor(x, y) {
+  // startX,startY  = start positions
+  constructor() {
     this.step = 5;
+    this.angle = 0;
     this.NDo = floor(random(360));
     this.radius = 100;
-    this.sx = (width / 2) + (this.radius * cos(this.NDo));
-    this.sy = (height / 2) + (this.radius * sin(this.NDo));
-    this.x = x;
-    this.y = y;
+
+    this.startX = (width / 2) + (this.radius * cos(this.NDo));
+    this.startY = (height / 2) + (this.radius * sin(this.NDo));
+    this.x = this.startX;
+    this.y = this.startY;
+    this.oldX = this.x;
+    this.oldY = this.y;
   }
 
-  controlIris(x, y) {
+  controlIris() {
     let isOutside = false;
 
     // what happens when x and y hit the outside
-    if (x < -2) isOutside = true;
-    else if (x > width + 2) isOutside = true;
-    else if (y < -2) isOutside = true;
-    else if (y > height + 2) isOutside = true;
+    if (this.x < -2) isOutside = true;
+    else if (this.x > width + 2) isOutside = true;
+    else if (this.y < -2) isOutside = true;
+    else if (this.y > height + 2) isOutside = true;
 
     return isOutside;
   }
 
   drawIris() {
-    let x = this.x;
-    let y = this.y;
+    this.angle = noise(this.x / noiseScale, this.y / noiseScale) * noiseStrength;
 
-    angle = noise(x / noiseScale, y / noiseScale) * noiseStrength;
+    // Radius change for every cycle
+    this.radius = this.rGen();
 
-    let ox = x;
-    let oy = y;
+    // Caclculate new X and Y position
+    this.x += cos(this.angle) * this.step;
+    this.y += sin(this.angle) * this.step;
 
-    radius = this.rGen();
-
-    x += cos(angle) * this.step;
-    y += sin(angle) * this.step;
-
-
-    if (this.controlIris(x, y)) {
-      x = ox;
-      y = oy;
+    // What happens when x and y hit the outside
+    if (this.controlIris()) {
+      this.x = this.oldX;
+      this.y = this.oldY;
     }
-  }
-
-  printIris() {
-    for (let i = 0; i < circles.length; i++) {
-
-      let circle = circles[i];
-      // display it
-      noFill();
-      stroke(cF, irisAlpha);
-      strokeWeight(strokeWidth);
-      line(circle.ox, circle.oy, circle.x, circle.y);
-    }
-    // return boolean to false for next cycle
-    isOutside = false;
+    // Display it
+    noFill();
+    stroke(255, 0, 255);
+    strokeWeight(strokeWidth);
+    ellipse(width/2, height/2, 90);
+    line(this.oldX, this.oldY, this.x, this.y);
   }
 
   reDrawIt() {
@@ -80,13 +64,14 @@ class Iris {
     noiseDetail(int(random(1, 10)), 0.5);
 
     // parameters reset
-    this.x = this.sx;
-    this.y = this.sy;
     this.NDo = int(random(360));
-    this.sx = width / 2 + radius * cos(NDo);
-    this.sy = height / 2 + radius * sin(NDo);
-    this.ox = this.x;
-    this.oy = this.y;
+    this.startX = (width / 2) + (this.radius * cos(this.NDo));
+    this.startY = (height / 2) + (this.radius * sin(this.NDo));
+
+    this.x = this.startX;
+    this.y = this.startY;
+    this.oldX = this.x;
+    this.oldY = this.y;
   }
 
   rGen() {
--- 2.Projects/07.Organic-Iris/sketch.js
+++ 2.Projects/07.Organic-Iris/sketch.js
@@ -1,4 +1,4 @@
-let arrIris = new Array(1000)
+let arrIris = [];
 
 // noise display factors
 let overlayAlpha = 0;
@@ -8,8 +8,12 @@ let strokeWidth = 0.3;
 // main circle parameters
 let rTemp = 100;
 let radius = 100
+
+let noiseScale = 1500;
+let noiseStrength = 250;
+
 // animation related variables
-let limit = 200;
+let limit = 100;
 let timer = 0;
 
 // colors
@@ -18,37 +22,40 @@ let bckg;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  smooth();
+  // smooth();
   bckg = color(0,0,0);
   c1 = color(255,255,255);
   background(bckg);
+
+  for (let i = 0; i < 3000; i++){
+    arrIris.push(new Iris());
+  }
 }
 
 function draw() {
-  for (let i = 0; i < arrIris.length; i++) {
-    arrIris[i] = new Iris();
-    timer += 0.5;
+  timer += 0.5;
+  // background related
+  fill(bckg, overlayAlpha);
+  rect(-5, -5, width + 10, height + 10);
 
-    // background related
-    fill(bckg, overlayAlpha);
+  if (timer > limit - 20) {
+    // this is for that quick fade at the end of a cycle
+    fill(bckg, overlayAlpha + 40);
     rect(-5, -5, width + 10, height + 10);
-    if (timer > limit - 20) {
-      // this is for that quick fade at the end of a cycle
-      fill(bckg, overlayAlpha + 40);
-      rect(-5, -5, width + 10, height + 10);
-    }
 
-    // Animate Iris
-    fill(c1);
+  }
+
+  // Animate Iris
+  fill(c1);
+  for (let i = 0; i < arrIris.length; i++) {
+    arrIris[i].drawIris();
+  }
+
+  // reset parameters every time 'limit' is hit
+  if (timer % limit === 0) {
     for (let i = 0; i < arrIris.length; i++) {
-      arrIris[i].drawIris();
-    }
-    // reset parameters every time 'limit' is hit
-    if (timer % limit === 0) {
-      for (let i = 0; i < arrIris; i++) {
-        arrIris[i].reDrawIt();
-      }
+      arrIris[i].reDrawIt();
     }
   }
 }
-  ///////////////////////
+///////////////////////
