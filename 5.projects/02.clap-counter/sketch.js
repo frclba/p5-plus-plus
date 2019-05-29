ce549a1e221e34f3a7ae5c07278921eba866d913
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-05-29 01:21:42 -0300

    Update 02.clap counter sketch

--- 5.projects/02.clap-counter/sketch.js
+++ 5.projects/02.clap-counter/sketch.js
@@ -7,17 +7,17 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	
 	clap.setVolume(0.3);
-	
-	
-}
-
-function draw() {
 	fill(255, 0, 255);
-	strokeWeight(3);
+	strokeWeight(3);	
 	for (i = 0; i < 252; i++) {
 		setInterval(() => {
 			clap.play()
-			ellipse(width / 2, height / 2, (60 % i) * 10 );
+			ellipse(width / 2, height / 2, (60+i));
+			console.log(i)
 		}, 100);
 	}
+}
+
+function draw() {
+
 }
