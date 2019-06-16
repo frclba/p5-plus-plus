97e6b117db55b0fd99421ac6ebc8c8f187488d14
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-06-16 17:41:02 -0300

    Update 05-06.global_warming

--- 5.projects/06.global_warming/sketch.js
+++ 5.projects/06.global_warming/sketch.js
@@ -1,22 +1,69 @@
+
 async function getData() {
 	const response = await fetch('ZonAnn.Ts.csv');
 	const data = await response.text();
-	console.log(data);
+	const years = [];
+	const temps = [];
+	const northern = [];
+	const southern = [];
 
-	const rows = data.split('\n').slice(0);
-	rows.forEach(element => {
-		const row = element.split(',');
-		const year = row[0];
-		const temp = row[1];
-		console.log(year, temp);
+	const rows = data.split('\n').slice(1);
+	rows.forEach(row => {
+		const cols = row.split(',')
+		years.push(cols[0]);
+		temps.push(14 + parseFloat(cols[1]));
+		northern.push(14 + parseFloat(cols[2]));
+		southern.push(14 + parseFloat(cols[3]));
 	});
+	return { years, temps, northern, southern };
 }
 
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	getData();
-}
-
-function draw() {
-
+async function setup() {
+	const context = document.getElementById('myChart').getContext('2d');
+	const dataTemps = await getData();
+	const myChart = new Chart(context, {
+		type: 'line',
+          data: {
+            labels: dataTemps.years,
+            datasets: [
+              {
+                label: 'Global Temperature in °C',
+                data: dataTemps.temps,
+                fill: false,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 1
+              },
+              {
+                label: 'Northern Hemisphere Temperature in °C',
+                data: dataTemps.northern,
+                fill: false,
+                borderColor: 'rgba(99, 132, 255, 1)',
+                backgroundColor: 'rgba(99, 132, 255, 0.5)',
+                borderWidth: 1
+              },
+              {
+                label: 'Souther Hemisphere in °C',
+                data: dataTemps.southern,
+                fill: false,
+                borderColor: 'rgba(99, 255, 132, 1)',
+                backgroundColor: 'rgba(99, 255, 132, 0.5)',
+                borderWidth: 1
+              }
+            ]
+		  },
+		  options: {
+			  scales: {
+				yAxes: [
+					{
+						ticks: {
+							callback: function(value, index, values) {
+								return value + `º`
+							}
+						}
+					}
+				]
+			  }
+		  }
+	});
 }
