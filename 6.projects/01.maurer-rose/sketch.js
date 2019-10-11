a926699fd027899375dac76ac89a77e7a3efce62
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-10-11 19:32:51 -0300

    Update 6.01 - maurer-rose to show better animation

--- 6.projects/01.maurer-rose/sketch.js
+++ 6.projects/01.maurer-rose/sketch.js
@@ -5,22 +5,23 @@ let dSlider;
 let nSlider;
 
 function setup() {
-	createCanvas(400, 400);
-	angleMode(DEGREES);
+	createCanvas(500, 500);
 	dSlider = createSlider(1, 180, 71);
 	nSlider = createSlider(1, 36, 6);
+	angleMode(DEGREES);
+	frameRate(1);
 }
 
 function draw() {
 	background(51);
-	translate(width/2, height/2);
+	translate(width/2, height/2 + 20);
 	noFill();
-	n = nSlider.value();
-	d = dSlider.value();
+	// n = nSlider.value();
+	// d = dSlider.value();
 
 	beginShape();
 	stroke(255, 0, 255);
-	strokeWeight(1);
+	strokeWeight(2);
 	for(let i = 0; i < 361; i++) {
 		let k = i * d;
 		let r = 210 * sin(n*k);
@@ -46,4 +47,6 @@ function draw() {
 	}
 	endShape();
 
+	n === 12 ? n = 1 : n += 1;
+	d === 100 ? d = 1 : d += 10;
 }
