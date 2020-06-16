b914939a7022036f4d780a22448c938c1ededf11
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-06-16 15:35:12 -0300

    1.01: Evolve Fractal Tree

--- 1.Projects/01.fractal_tree/sketch.js
+++ 1.Projects/01.fractal_tree/sketch.js
@@ -1,36 +1,47 @@
+let hue = 0;
+let scale = 0.333;
+
 function setup() {
-	slider = createSlider(0, PI, PI/4, 0.01);
-	canvas = createCanvas(windowWidth, windowHeight);
+	slider = createSlider(0.1, 2.36, PI/4, 0.0001);
+	createCanvas(windowWidth, windowHeight*0.8);
 }
 
 function draw() {
-	canvas.show();
+	colorMode(HSB, 255);
 	background(51);
+
 	angle = slider.value();
-	stroke(255);
+	
+	hue = hue > 255 ? 0 : hue += 3
+	stroke(hue, 255, 255);
 	strokeWeight(2);
 	noFill();
+	
 	translate(width/2, height);
-	branch(150);
+	branch(floor(height*scale));
 }
 
 function branch (len) {
-	var fraction = 2/3;
+	var fraction = 0.6666666;
 
 	line(0, 0, 0, -len);
 	translate(0, -len);
 
-	if(len > 3) {
+	const branch_size = len*fraction
+	if(len > 6) {
+		ellipse(0, 0, len*0.3);
+
 		// Draw Left Branch
 		push();
 		rotate(angle);
-		branch(len*fraction);
+		branch(branch_size);
 		pop();
 
 		// Draw Right Branch
 		push();
 		rotate(-angle);
-		branch(len*fraction);
+		branch(branch_size);
+		// drawCircle(0,0, 6); //cpu intensive - slows down too much
 		pop();
 	}
 }
@@ -38,7 +49,7 @@ function branch (len) {
 function drawCircle(x, y, radius){
 	ellipse(x, y, radius);
 
-	if (radius > 1 * 2){
+	if (radius > 2){
 		// drawCircle(x + radius * 0.5, y, radius * 0.5);
 		// drawCircle(x - radius * 0.5, y, radius * 0.5);
 		if (random(0, 10) < 20){
