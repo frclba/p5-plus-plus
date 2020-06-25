07bedfc3e0c43b6613dfc5da6f3a38a3da3be3f9
Fernando Ribeiro Aguilar <fernando.aguilar@hotmail.com.br>
2020-06-25 02:35:07 -0300

    6.01 - Update for mobile version

--- 6.projects/01.maurer-rose/sketch.js
+++ 6.projects/01.maurer-rose/sketch.js
@@ -3,11 +3,14 @@ let d = 0;
 
 let dSlider;
 let nSlider;
+let checkbox;
 
 function setup() {
-	createCanvas(600, 600);
 	dSlider = createSlider(1, 180, 72);
 	nSlider = createSlider(1, 36, 6);
+  	checkbox = createCheckbox('Enable control', false);
+
+	createCanvas(windowWidth, windowHeight);
 	angleMode(DEGREES);
 	frameRate(30);
 }
@@ -16,15 +19,21 @@ function draw() {
 	background(51);
 	translate(width/2, height/2 + 20);
 	noFill();
-	// n = nSlider.value();
-	// d = dSlider.value();
-
+	
+	if(checkbox.checked()){
+		n = nSlider.value();
+		d = dSlider.value();
+	} else {
+		n === 10 ? n = 1 : n += 0.01;
+		d === 30 ? d = 1 : d += 0.01;
+	}
+	
 	beginShape();
 	stroke(255, 0, 255);
 	strokeWeight(2);
 	for(let i = 0; i < 361; i++) {
 		let k = i * d;
-		let r = 210 * sin(n*k);
+		let r = 77 * sin(n*k);
 
 		let x = r * cos(k);
 		let y = r * sin(k);
@@ -38,7 +47,7 @@ function draw() {
 	strokeWeight(3);
 	for(let i = 0; i < 361; i++) {
 		let k = i;
-		let r = 210 * sin(n*k);
+		let r = 70 * sin(n*k);
 
 		let x = r * cos(k);
 		let y = r * sin(k);
@@ -46,7 +55,4 @@ function draw() {
 		vertex(x, y);
 	}
 	endShape();
-
-	n === 10 ? n = 1 : n += 0.01;
-	d === 30 ? d = 1 : d += 0.01;
 }
